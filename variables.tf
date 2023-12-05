65990dfbc46cd07e3707819e18f0aa613bd3259f
root <cristiano.rosetti@gmail.com>
2023-12-05 22:30:52 +0000

    init

--- variables.tf
+++ variables.tf
@@ -4,10 +4,13 @@ variable "ecs_task_name" {}
 variable "ecs_task_image" {}
 variable "ecs_service_name" {}
 variable "ecs_cluster_id" {}
-variable "ecs_service_name" {}
 variable "ecs_capacity_provider" {}
 
-variable "vpc_subnets" {}
-variable "vpc_security_group_ids" {}
+variable "vpc_subnets" {
+  type = list(string)
+}
+variable "vpc_security_group_ids" {
+  type = list(string)
+}
 
 variable "aws_lb_target_group" {}
